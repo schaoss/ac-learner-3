b148af4bc7b2bfcdc9a846f0f9e6101f7b5567d5
gary.chu <gary_chu@owlting.com>
2020-04-03 16:25:42 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.get('/', (req, res) => {
   // 拿到全部的 Todo 資料
   Todo.find()
     .lean()
+    .sort({ _id: 'asc' }) // desc
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
 })
