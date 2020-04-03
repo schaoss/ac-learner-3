3f3e00aa285fbc35257b9644ecc4097bda3a1e3a
gary.chu <gary_chu@owlting.com>
2020-04-03 14:12:11 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('hello world!')
