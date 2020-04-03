e6f81b6b6d45820e82d03278e59debe8140f2e36
gary.chu <gary_chu@owlting.com>
2020-04-03 15:31:07 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -46,6 +46,14 @@ app.post('/todos', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then(todo => res.render('detail', { todo }))
+    .catch(error => console.log(error))
+})
+
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
