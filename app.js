47f191a785e33e43199e5323a6e040b41f2bf62d
gary.chu <gary_chu@owlting.com>
2020-04-03 17:53:51 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,37 +1,17 @@
 const express = require('express')
-const mongoose = require('mongoose')
-
 const exphbs = require('express-handlebars')
-
 const bodyParser = require('body-parser')
-
 const methodOverride = require('method-override')
 
-const Todo = require('./models/todo')
-
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(methodOverride('_method'))
-
 
 app.use(routes)
 
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,20 +1,9 @@
-const mongoose = require('mongoose')
 const Todo = require('../todo')
-
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  console.log('mongodb connected!')
-
   for (let i = 0; i < 10; i++) {
     Todo.create({ name: `name-${i}`})
   }
-
   console.log('done.')
 })
