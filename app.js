46c5301e841429fa2d5969d390c78427533688a5
gary.chu <gary_chu@owlting.com>
2020-04-03 16:03:16 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -74,6 +74,14 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
